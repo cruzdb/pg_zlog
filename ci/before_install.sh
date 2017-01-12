8b647d96cec322d4c9bcc436d519af99ab8f2c55
Noah Watkins <noahwatkins@gmail.com>
2017-01-12 12:41:43 -0800

    wip
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -3,6 +3,13 @@
 set -e
 set -x
 
+sudo apt-get update
+sudo apt-get install -y python-virtualenv
+
+ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
 WORKDIR=$HOME/workdir
 mkdir $WORKDIR
 pushd $WORKDIR
@@ -10,6 +17,9 @@ pushd $WORKDIR
 git clone git://github.com/ceph/ceph-deploy
 pushd ceph-deploy
 ./bootstrap
+
+./ceph-deploy install --release kraken `hostname`
+./ceph-deploy pkg --install librados-dev `hostname`
 
 popd # ceph-deploy
 popd # workdir
